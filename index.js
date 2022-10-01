a802382487d31ddb4ca0bbc11dc472782a881fdc
Igor Q <igorqueirozdossantoslima_@hotmail.com>
2022-10-01 17:05:43 -0300

    aula de fetch

---
+++ index.js
@@ -0,0 +1,37 @@
+function retornoPromessaFetch(resposta) {
+    let promessaBody = resposta.json();
+    return promessaBody;
+}
+
+function retornoPromessaBody(body) {
+    lista.innerHTML = ""
+    body.forEach(function (pessoa) {
+        console.log(pessoa)
+
+        lista.innerHTML = lista.innerHTML + "<li>" + pessoa.nome + "</li>"
+    });
+}
+
+
+function atualizar() {
+    let promessa = fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa')
+    promessa.then(retornoPromessaFetch).then(retornoPromessaBody)
+}
+
+function adicionar() {
+    let nomeDaPessoa = nome.value;
+    console.log(nomeDaPessoa);
+    let requestPost = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            idade: 20,
+            nome: nomeDaPessoa,
+        })
+    }
+    fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa', requestPost).then(function (resposta) {
+        console.log(resposta)
+    })
+}
